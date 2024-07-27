8af03aaedbb962c6eb3618e78181df944099a377
fadil <fadilshereefmx@gmail.com>
2024-07-27 12:53:30 +0530

    Update add.jsx

--- admin/src/pages/add/add.jsx
+++ admin/src/pages/add/add.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const add = () => {
-  const url = "https://food-delivery-app-backend-kwz5.onrender.com";
+  const url = "https://food-delivery-backend-slp5.onrender.com";
   const [image, setImage] = React.useState(false);
   const [data, setData] = React.useState({
     name: "",
