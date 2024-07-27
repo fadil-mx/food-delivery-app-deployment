e845247d0005623b61262e99ca0ca63b27e3ce0f
fadil <fadilshereefmx@gmail.com>
2024-07-27 12:27:14 +0530

    Update list.jsx

--- admin/src/pages/list/list.jsx
+++ admin/src/pages/list/list.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const List = () => {
-  const url = "http://localhost:3000";
+  const url = "https://food-delivery-app-backend-kwz5.onrender.com";
   const [list, setList] = useState([]);
   
 
