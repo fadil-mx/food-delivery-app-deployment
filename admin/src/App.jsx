dbc7cf0175c80ce9691df1b1ed494dec45019b08
fadil <fadilshereefmx@gmail.com>
2024-07-27 12:24:46 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url = "http://localhost:3000";
+  const url = "adil-mx/food-delivery-app-using-mernstack";
 
   return (
     <div>
